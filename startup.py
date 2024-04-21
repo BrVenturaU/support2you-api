748109d3f0181c34f335a236ae411ed45a8004da
BrVenturaU <brandonventura16@gmail.com>
2024-04-21 13:12:54 -0600

    Bugfix providers load when config file is empty.

--- startup.py
+++ startup.py
@@ -13,12 +13,14 @@ def configure_services(container: DependencyContainer):
 def add_app_configuration(container: DependencyContainer):
     APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT")
 
-    # Default app configurations
-    container.configuration.from_yaml(f"config.yaml")
+    try:
+        # Default app configurations
+        container.configuration.from_yaml(f"config.yaml")
 
-    # Environmental app configurations
-    config_filename = f"config.{APP_ENVIRONMENT}.yaml"
-    container.configuration.from_yaml(config_filename)
-
+        # Environmental app configurations
+        config_filename = f"config.{APP_ENVIRONMENT}.yaml"
+        container.configuration.from_yaml(config_filename)
+    except Exception:
+        pass # TODO: Add logguer
     # Environment and constant value configurations
     container.configuration.env.from_value(APP_ENVIRONMENT)
