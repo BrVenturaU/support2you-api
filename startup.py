4b0dd3881cc9641d321093492f41c102c636d05a
BrVenturaU <brandonventura16@gmail.com>
2024-04-21 11:11:14 -0600

    Add app configuration providers.
    - Add environmental configuration files.
    - Provide env variable example.
    - Apply configuration providers.

--- startup.py
+++ startup.py
@@ -1,3 +1,4 @@
+import os
 from dependency_injector import containers, providers
 
 class DependencyContainer(containers.DeclarativeContainer):
@@ -10,4 +11,14 @@ def configure_services(container: DependencyContainer):
     pass # Configure implementations of Services
 
 def add_app_configuration(container: DependencyContainer):
-    pass # Add app configuration.
+    APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT")
+
+    # Default app configurations
+    container.configuration.from_yaml(f"config.yaml")
+
+    # Environmental app configurations
+    config_filename = f"config.{APP_ENVIRONMENT}.yaml"
+    container.configuration.from_yaml(config_filename)
+
+    # Environment and constant value configurations
+    container.configuration.env.from_value(APP_ENVIRONMENT)
