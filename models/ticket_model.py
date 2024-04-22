b3d65a49e97e5f19f3184527c16ae83a2c7f1686
BrVenturaU <brandonventura16@gmail.com>
2024-04-21 21:17:56 -0600

    Add ticket database model class.

---
+++ models/ticket_model.py
@@ -0,0 +1,19 @@
+import enum
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from data.database import Base
+
+
+class TicketStatus(enum.Enum):
+    NUEVO = 1
+    ABIERTO = 2
+    PENDIENTE = 3
+    ESPERA = 4
+    RESUELTO = 5
+
+class Ticket(Base):
+    __tablename__ = "Tickets"
+
+    Id: Mapped[int] = mapped_column(primary_key=True)
+    Status: Mapped[TicketStatus] = mapped_column(Enum(TicketStatus))
