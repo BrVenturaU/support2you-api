32421a8581445257596aa604ac59f0e8bc3926b0
BrVenturaU <brandonventura16@gmail.com>
2024-04-22 09:50:51 -0600

    Add base response model schema.
    - Use response model to standardize responses from the api.

--- features/tickets/create_ticket.py
+++ features/tickets/create_ticket.py
@@ -1,19 +1,20 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from dependency_injector.wiring import inject, Provide
 from pydantic import BaseModel
 
 from data.database import Database
 from models.ticket_model import Ticket
 from startup import DependencyContainer
+from schema.base import ApiResponse
 
 router = APIRouter()
 
 class CreatedTicketResponse(BaseModel):
     id: int
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 @inject
-def create_ticket(db: Database = Depends(Provide[DependencyContainer.db])) -> CreatedTicketResponse:
+def create_ticket(db: Database = Depends(Provide[DependencyContainer.db])) -> ApiResponse[CreatedTicketResponse]:
     with db.session() as session:
         ticket = Ticket()
         session.add(ticket)
@@ -21,4 +22,8 @@ def create_ticket(db: Database = Depends(Provide[DependencyContainer.db])) -> Cr
 
         ticket_id = ticket.Id
         session.commit()
-        return CreatedTicketResponse(id=ticket_id)
+        return ApiResponse(
+            message="El ticket ha sido creado exitosamente.",
+            code=status.HTTP_201_CREATED,
+            data=CreatedTicketResponse(id=ticket_id)
+        )
--- features/tickets/get_all_tickets.py
+++ features/tickets/get_all_tickets.py
@@ -5,6 +5,7 @@ from dependency_injector.wiring import inject, Provide
 from data.database import Database
 from models.ticket_model import Ticket
 from startup import DependencyContainer
+from schema.base import ApiResponse
 
 class TicketResponse(BaseModel):
     id: int
@@ -14,8 +15,12 @@ router = APIRouter()
 
 @router.get("/")
 @inject
-def get_all_tickets(db: Database = Depends(Provide[DependencyContainer.db])) -> list[TicketResponse]:
+def get_all_tickets(db: Database = Depends(Provide[DependencyContainer.db])) -> ApiResponse[list[TicketResponse]]:
     with db.session() as session:
         tickets = session.query(Ticket).all()
-        return [TicketResponse(id=el.Id, status=el.Status.name) for el in tickets]
+        tickets_response = [TicketResponse(id=el.Id, status=el.Status.name) for el in tickets]
+        return ApiResponse(
+            message="Listado de tickets obtenido correctamente.",
+            data=tickets_response
+        )
         
