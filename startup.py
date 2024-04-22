764b9c983adf97ecb81ad41ec7b53d7e37453765
BrVenturaU <brandonventura16@gmail.com>
2024-04-21 20:33:19 -0600

    Bugfix circular import.
    The startup file was importing controller modules, and controller modules will have a reference to the container to get dependencies, this causes a circular import between modules. To solve it, every related functionality to the API is being moved to the main module.

--- startup.py
+++ startup.py
@@ -1,11 +1,7 @@
 import os
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from dependency_injector import containers, providers
 
 from data.database import Database
-from features.tickets import tickets_controller
-from features.messages import messages_controller
 
 class DependencyContainer(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=[])
@@ -33,17 +29,3 @@ def add_configuration_providers(container: DependencyContainer):
     container.configuration.env.from_value(APP_ENVIRONMENT)
     container.configuration.connection_string.from_env('APP_DB_CONNECTION')
 
-def configure_pipeline(app: FastAPI) -> FastAPI:
-    # Middlewares
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    # Routes
-    app.include_router(tickets_controller.router)
-    app.include_router(messages_controller.router)
-
-    return app
