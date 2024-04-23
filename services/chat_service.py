bcf02b3576139e2f2a75fc7236888f858ae3d65f
BrVenturaU <brandonventura16@gmail.com>
2024-04-22 19:26:48 -0600

    Add base class for chat service.
    - Resolve chat service based on environment running.
    - Configure abstract factory for services.
    - Add new configuration values to know whether is dev or prod env.

--- services/chat_service.py
+++ services/chat_service.py
@@ -1,5 +1,5 @@
 import openai
-
+from abc import ABC, abstractmethod
 from models.message_model import MessageRole
 
 
@@ -10,21 +10,44 @@ class PromptMessage:
         pass
 
 
-class ChatService:
+class BaseChatService(ABC):
+    @abstractmethod
+    def answer(self, messages: list[PromptMessage]):
+        pass
+
+
+class ChatService(BaseChatService):
     def __init__(self, sys_prompt, model, temperature):
         self.sys_prompt = PromptMessage(MessageRole.system, sys_prompt)
         self._model = model
         self._temperature = temperature
 
     def answer(self, messages: list[PromptMessage]):
-        prompt = [vars(el) for el in messages]
-        prompt.insert(0, vars(self.sys_prompt))
+        prompt = [{"role": el.role.value, "content": el.content} for el in messages]
+        prompt.insert(
+            0, {"role": self.sys_prompt.role.value, "content": self.sys_prompt.content}
+        )
 
-        # completion = openai.ChatCompletion.create(
-        #     model=self.__model, messages=prompt, temperature=self._temperature
-        # )
+        completion = openai.ChatCompletion.create(
+            model=self._model, messages=prompt, temperature=self._temperature
+        )
 
-        # completion_answer = completion.choices[0].message.content
-        completion_answer = "Temporary system generated message."
+        completion_answer = completion.choices[0].message.content
+
+        return completion_answer
+
+
+class MockedChatService(BaseChatService):
+    def __init__(self, sys_prompt, model, temperature):
+        self.sys_prompt = PromptMessage(MessageRole.system, sys_prompt)
+        self._model = model
+        self._temperature = temperature
+
+    def answer(self, messages: list[PromptMessage]):
+        [*prompt, last_message] = messages
+
+        completion_answer = (
+            f"Temporary system generated message: You said: {last_message.content}."
+        )
 
         return completion_answer
